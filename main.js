a06c64658300d6c36bd52fecd3d4a29d042c086d
Silvaninha <137855273+SilvinhaaBraba@users.noreply.github.com>
2023-10-19 09:36:49 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,33 @@
-const fetch = require('node-fetch'); // Se estiver usando Node.js
+document.addEventListener('DOMContentLoaded', () => {
+    const searchButton = document.getElementById('searchButton');
+    const searchNameInput = document.getElementById('searchName');
+    const contactInfoDiv = document.getElementById('contactInfo');
 
-// Passo 1: Obter o token de acesso
-const authUrl = 'https://api.rd.services/auth/token';
-const clientId = 'your_client_id';
-const clientSecret = 'your_client_secret';
-const authData = { client_id: clientId, client_secret: clientSecret };
+    searchButton.addEventListener('click', () => {
+        const nameToSearch = searchNameInput.value;
+        const apiEndpoint = 'https://crm.rdstation.com/api/v1/contacts';
+        const accessToken = 'seu_token_de_acesso'; // Substitua pelo seu token de acesso
 
-fetch(authUrl, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(authData),
-})
-  .then((response) => response.json())
-  .then((data) => {
-    const accessToken = data.access_token;
-    // Passo 2: Fazer uma chamada à API usando o token de acesso
-    const apiEndpoint = 'https://crm.rdstation.com/api/v1/contacts';
-    fetch(apiEndpoint, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${accessToken}`,
-        'accept': 'application/json',
-      },
-    })
-      .then((response) => response.json())
-      .then((contactData) => {
-        console.log(contactData);
-        // Aqui você pode manipular os dados dos contatos e exibi-los na página
-      })
-      .catch((error) => console.error('Erro na chamada à API:', error));
-  })
-  .catch((error) => console.error('Erro na obtenção do token de acesso:', error));
+        // Fazer a chamada à API usando o nome do contato
+        fetch(`${apiEndpoint}?name=${nameToSearch}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+                'accept': 'application/json',
+            },
+        })
+        .then((response) => response.json())
+        .then((contactData) => {
+            // Verificar se o contato foi encontrado
+            if (contactData.length > 0) {
+                // Exibir as informações do primeiro contato encontrado
+                const contatoEncontrado = contactData[0];
+                contactInfoDiv.innerText = `Nome: ${contatoEncontrado.name}, Email: ${contatoEncontrado.email}, Telefone: ${contatoEncontrado.phone}`;
+            } else {
+                // Se o contato não foi encontrado, exibir uma mensagem
+                contactInfoDiv.innerText = 'Contato não encontrado.';
+            }
+        })
+        .catch((error) => console.error('Erro na chamada à API:', error));
+    });
+});
